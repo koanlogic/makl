dec55370b60de71a556888e06302274eb2bca06b
tho <tho>
2005-08-02 10:10:24 +0000

    a less error-prone parser :-)

--- setup/toolchain_setup.sh
+++ setup/toolchain_setup.sh
@@ -3,6 +3,7 @@
 # With no arguments the script tries to divine platform and toolchain file.  
 # If an argument is supplied it is interpreted as the toolchain file to install.
 #
+# $Id: toolchain_setup.sh,v 1.5 2005/08/02 10:10:24 tho Exp $
 
 if [ -z ${MAKL_DIR} ]; then
     echo "set MAKL_DIR in the shell environment before running any MaKL script"
@@ -13,15 +14,20 @@ for f in ${MAKL_DIR}/cf/makl_utils ${MAKL_DIR}/tc/makl_tc ; do
     . $f
 done
 
+rsname=`uname -rs | tr [A-Z] [a-z] | sed -e 's/ //'`
+
 if [ $# -ne 0 ]; then
     tc_file=$1 
 else
-    case `uname -s`
+    case ${rsname}
     in 
-        "FreeBSD")
+        freebsd*)
             platform="freebsd"
             ;;
-        "Darwin")
+        linux*)
+            platform="linux"
+            ;;
+         darwin*)
             platform="darwin"
             ;;
         *)
