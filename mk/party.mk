009085706ccaf32fe13828e5a363e812c37a26e4
tho <tho>
2005-11-27 21:48:56 +0000

    [makl] party.mk now includes map.mk

--- mk/party.mk
+++ mk/party.mk
@@ -1,11 +1,11 @@
 #
-# $Id: party.mk,v 1.2 2005/11/25 14:55:54 tho Exp $
+# $Id: party.mk,v 1.3 2005/11/27 21:48:56 tho Exp $
 # 
 # User Variables:
-# - PARTY_BASE	The name of the 3rd party package
-# - PARTY_DEP		Dependency file used to determine whether target is up to date
-# - PARTY_CONF	Configure script to be used
-# - PARTY_ARGS	Arguments to be passed to configure script
+# - PARTY_BASE  The name of the 3rd party package
+# - PARTY_DEP   Dependency file used to determine whether target is up to date
+# - PARTY_CONF  Configure script to be used
+# - PARTY_ARGS  Arguments to be passed to configure script
 #
 # Applicable Targets:
 # - party
@@ -27,3 +27,5 @@ party: ${PARTY_DEP}
 
 # set standard MaKL targets even if they don't do nothing
 clean install uninstall depend cleandepend:
+
+include ../etc/map.mk
