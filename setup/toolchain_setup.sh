24c81860c4b29fafef26c0d30d6b8d330883c9a1
tho <tho>
2006-03-06 16:38:06 +0000

    [makl] setup_toolchain.sh nuts

--- setup/toolchain_setup.sh
+++ setup/toolchain_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-# With no arguments the script tries to divine platform and toolchain file.  
-# If an argument is supplied it is interpreted as the toolchain file to install.
+# If MAKL_TC is set then use that toolchain file, otherwise look at the command
+# line arguments, as a last resort try to guess the platform.
 #
-# $Id: toolchain_setup.sh,v 1.17 2006/03/06 16:15:18 tat Exp $
+# $Id: toolchain_setup.sh,v 1.18 2006/03/06 16:38:06 tho Exp $
 
-KL_COM=http://www.koanlogic.com/
+KL_WEBSITE=http://www.koanlogic.com/
 
 if [ -z ${MAKL_DIR} ]; then
     echo "set MAKL_DIR in the shell environment before running any MaKL script"
@@ -18,13 +18,17 @@ done
 
 rsname=`uname -rs | tr [A-Z] [a-z] | sed -e 's/ //'`
 
-# if MAKL_TC is set then use that toolchain file otherwise look to the command
-# line args otherwise try to guess the platform
 if [ "${MAKL_TC}" ]; then
     tc_file=${MAKL_TC}
 else
+    # if we've been passed a specific platform, use it.
     if [ $# -ne 0 ]; then
         tc_file=${MAKL_DIR}/tc/$1.tc
+        # if the toolchain does not exists try to download it from KL web site
+        if [ ! -f "${tc_file}" ]; then
+            (cd ${MAKL_DIR}/tc/ && \
+             ${FETCH:=wget} ${KL_COM}/makl/`basename ${tc_file}`)
+        fi
     else
         case ${rsname}
         in 
@@ -52,15 +56,8 @@ else
         esac        
 
         tc_file=${MAKL_DIR}/tc/${platform}.tc
-
-    fi
-
-    # if the toolchain does not exists try to download from kl.com/makl/*.tc
-    if [ ! -f ${tc_file} ]; then
-        (cd ${MAKL_DIR}/tc/ && ${FETCH:=wget} ${KL_COM}/makl/`basename ${tc_file}`) 
     fi
 fi
-
 
 echo "MaKL: installing toolchain file '${tc_file}'"
 
