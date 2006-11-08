b459ca6984cdb7e01dc013a0dfa16f02692b070a
tho <tho>
2006-11-08 13:39:58 +0000

    [makl] added netbsd shlib file

--- setup/shlib_setup.sh
+++ setup/shlib_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: shlib_setup.sh,v 1.2 2006/11/06 09:41:00 tho Exp $
+# $Id: shlib_setup.sh,v 1.3 2006/11/08 13:39:58 tho Exp $
 
 if [ -z ${MAKL_DIR} ]; then
     echo "set MAKL_DIR in the shell environment before running any MaKL script"
@@ -19,6 +19,9 @@ else
     else
         case ${rsname}
         in 
+            netbsd*)
+                platform="netbsd"
+                ;;
             freebsd*)
                 platform="freebsd"
                 ;;
---
+++ shlib/netbsd.mk
@@ -0,0 +1,77 @@
+# $Id: netbsd.mk,v 1.1 2006/11/08 13:39:58 tho Exp $
+#
+# NetBSD
+
+ifdef SHLIB
+
+SHLIB_OBJS = $(OBJS:.o=.so)
+SHLIB_MAJOR ?= 0
+SHLIB_MINOR ?= 0
+
+# default object fmt
+OBJFORMAT ?= elf
+
+#
+# automatic rules for shared objects
+#
+.SUFFIXES: .so .c .cc .C .cpp .cxx
+
+.c.so:
+	$(CC) -fpic -DPIC $(CFLAGS) -c $< -o $*.so
+
+.cc.so .C.so .cpp.so .cxx.so:
+	$(CXX) -fpic -DPIC $(CXXFLAGS) -c $< -o $*.so
+
+#
+# set library naming vars (perhaps specific to OBJFORMAT)
+#
+ifeq ($(strip $(OBJFORMAT)), aout)
+    SHLIB_LINK ?= lib$(__LIB).so
+    SHLIB_NAME ?= $(SHLIB_LINK).$(SHLIB_MAJOR).$(SHLIB_MINOR)
+else
+ifeq ($(strip $(OBJFORMAT)), elf)
+    SHLIB_LINK ?= lib$(__LIB).so
+    SONAME ?= $(SHLIB_LINK).$(SHLIB_MAJOR)
+    SHLIB_NAME ?= $(SONAME).$(SHLIB_MINOR)
+else
+    $(error OBJFORMAT must be one of aout or elf on NetBSD platform)
+endif
+endif
+
+#
+# build rules
+#
+all-shared: $(SHLIB_NAME)
+
+$(SHLIB_NAME): $(SHLIB_OBJS)
+	@echo "===> building shared $(__LIB) library ($(OBJFORMAT))"
+	rm -f $(SHLIB_NAME) $(SHLIB_LINK)
+	ln -sf $(SHLIB_NAME) $(SHLIB_LINK)
+ifeq ($(strip $(OBJFORMAT)), aout)
+	$(CC) -shared -Wl,-x,-assert,pure-text \
+	    -o $(SHLIB_NAME) \
+	    `$(LORDER) $(SHLIB_OBJS) | $(TSORT)` $(LDADD) ${LDFLAGS} 
+else
+ifeq ($(strip $(OBJFORMAT)), elf)
+	$(AR) cq $(__LIB)_pic.a `$(LORDER) $(SHLIB_OBJS) | $(TSORT)`
+	$(LD) -x -shared -R$(SHLIBDIR) -soname $(SONAME) -o \
+	    $(SHLIB_NAME) /usr/lib/crtbeginS.o --whole-archive \
+	    $(__LIB)_pic.a /usr/lib/crtendS.o
+endif
+endif
+
+install-shared:
+	$(INSTALL) $(_INSTALL_ARGS) -m $(LIBMODE) $(SHLIB_NAME) $(LIBDIR)
+	ln -sf $(SHLIB_NAME) $(LIBDIR)/$(SHLIB_LINK)
+	ln -sf $(SHLIB_NAME) $(LIBDIR)/$(SONAME)
+
+uninstall-shared:
+	rm -f $(LIBDIR)/$(SHLIB_NAME)
+	rm -f $(LIBDIR)/$(SHLIB_LINK)
+	rm -f $(LIBDIR)/$(SONAME)
+
+clean-shared:
+	rm -f $(SHLIB_OBJS)
+	rm -f $(SHLIB_NAME) $(SHLIB_LINK)
+
+endif
