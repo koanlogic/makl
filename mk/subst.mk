434e350c101f535c9f8dbdbfa764c7cf867d46b1
tho <tho>
2006-11-30 16:57:48 +0000

    [makl] top level Makefile and mk/subst.mk: clean up and comments

--- mk/subst.mk
+++ mk/subst.mk
@@ -1,22 +1,25 @@
-# $Id: subst.mk,v 1.3 2006/11/30 00:31:52 tho Exp $
 #
-# targets: subst
+# $Id: subst.mk,v 1.4 2006/11/30 16:57:48 tho Exp $
 #
-# variables:    
-#   SUBST_RULE      substitution rule (see further on)
-#   SUBST_SUFFIX    if a substitution is requested to have output file X the 
+# User variables:    
+# - SUBST_RULE      substitution rule (see further on)
+# - SUBST_SUFFIX    if a substitution is requested to have output file X the 
 #                   input file is read from X.$(SUBST_SUFFIX)
 #
-# we have two different kind of SUBST_RULE, shortcut and full: if SUBST_SUFFIX 
+# Available targets: 
+#   subst
+#
+# We have two different kind of SUBST_RULE: shortcut and full.  If SUBST_SUFFIX 
 # is defined, then SUBST_RULE = <output_file> <"sed(1) command">, otherwise
 # SUBST_RULE = <input_file> <output_file> <"sed(1) command">.
 #
-# NOTE: a '$' sign in the sed(1) command must be escaped (i.e. '$$') if it 
-# wants to survive the GNU make variable expansion:
+# NOTE: a '$' sign in the sed(1) command must be escaped (i.e. '$$') if you
+# want it to survive the GNU make variable expansion, e.g.:
 #   => SUBST_RULE = A.in A.out "s/\!$$/\?/g" 
 # results into:
 #   => sed 's/\!$/\?/g' A.in > A.out
 
+# check preconditions
 ifndef SUBST_RULE
 subst:
 	$(warning SUBST_RULE must be defined when including subst.mk template !)
