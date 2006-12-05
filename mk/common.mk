d17b5709b1ac4fb13738e2f867ef3b91dfaf9121
tho <tho>
2006-12-05 09:39:02 +0000

    [makl] MAKL_TC_DIR added to common.mk for local toolchain override

--- mk/common.mk
+++ mk/common.mk
@@ -1,5 +1,5 @@
 #
-# $Id: common.mk,v 1.2 2006/11/10 09:24:07 tho Exp $
+# $Id: common.mk,v 1.3 2006/12/05 09:39:02 tho Exp $
 #
 # Common include, to be placed on top of your Makefile, so that the following
 # includes and variables' settings can override toolchain and map values.
@@ -7,4 +7,5 @@
 .DEFAULT_GOAL := all
 
 include ../etc/toolchain.mk
+-include $(MAKL_TC_DIR)/toolchain.mk
 include ../etc/map.mk
