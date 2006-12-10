60d0fdbe336561a84904772e73f8dd215d1f7c47
tho <tho>
2006-12-10 07:39:02 +0000

    [makl] cleanup in subdir.mk

--- mk/subdir.mk
+++ mk/subdir.mk
@@ -1,4 +1,4 @@
-# $Id: subdir.mk,v 1.9 2006/11/06 09:16:35 tho Exp $
+# $Id: subdir.mk,v 1.10 2006/12/10 07:39:02 tho Exp $
 #
 # Variables:
 # - SUBDIR      A list of subdirectories that should be built as well.
@@ -8,7 +8,12 @@
 # Applicable Targets:
 # - any target
 
-all $(MAKECMDGOALS):
+# if not specified, default target is "all"
+ifndef MAKECMDGOALS
+MAKECMDGOALS = all
+endif
+
+$(MAKECMDGOALS):
 	@for dir in $(SUBDIR) ; do \
 	    $(MAKE) -C $$dir $(MAKECMDGOALS) ; \
         [ $$? = 0 ] || exit $$? ; \
