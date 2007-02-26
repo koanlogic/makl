9454196f77374241be3c67e9ec16816ed08c303b
tat <tat>
2007-02-26 20:27:14 +0000

    [makl] ".DEFAULT_GOAL := all" removed from common.mk

--- mk/common.mk
+++ mk/common.mk
@@ -1,10 +1,8 @@
 #
-# $Id: common.mk,v 1.3 2006/12/05 09:39:02 tho Exp $
+# $Id: common.mk,v 1.4 2007/02/26 20:27:14 tat Exp $
 #
 # Common include, to be placed on top of your Makefile, so that the following
 # includes and variables' settings can override toolchain and map values.
-
-.DEFAULT_GOAL := all
 
 include ../etc/toolchain.mk
 -include $(MAKL_TC_DIR)/toolchain.mk
