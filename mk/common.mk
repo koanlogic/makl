c24bbcbafdc187090b74ffe31c6ebf455cf2b7f0
tho <tho>
2007-06-26 13:23:49 +0000

    [makl] nutshell tat's fixes

--- mk/common.mk
+++ mk/common.mk
@@ -1,11 +1,10 @@
 #
-# $Id: common.mk,v 1.6 2007/06/26 10:19:56 tho Exp $
+# $Id: common.mk,v 1.7 2007/06/26 13:23:49 tho Exp $
 #
 # Common include, to be placed on top of your Makefile, so that the following
 # includes and variables' settings can override toolchain and map values.
 
 MAKL_ETC ?= ../etc
 
-include ../etc/toolchain.mk
--include $(MAKL_ETC)/toolchain.mk
+include $(MAKL_ETC)/toolchain.mk
 include ../etc/map.mk
