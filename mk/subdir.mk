b4da2a2d32f8cdcb1b0e69c6d7a2606a6d558bfa
tho <tho>
2008-05-21 16:14:46 +0000

    [makl] subdir == subdir-basic, if hooks are needed include subdir-hooks explicitly (only supported when GNU make >= 3.81 is in use)

--- mk/subdir.mk
+++ mk/subdir.mk
@@ -1,4 +1,4 @@
-# $Id: subdir.mk,v 1.31 2008/05/21 12:31:01 tho Exp $
+# $Id: subdir.mk,v 1.32 2008/05/21 16:14:46 tho Exp $
 #
 # Variables:
 # - SUBDIR      A list of subdirectories that should be built as well.
@@ -19,14 +19,14 @@ ifeq ($(findstring .help, $(MAKECMDGOALS)),)
 .DEFAULT_GOAL := all
 MAKECMDGOALS ?= $(.DEFAULT_GOAL)
 
-# when using GNU make == 3.81, support target hooks, otherwise basic subdir
-# recursion is in place
-ifeq ($(MAKE_VERSION), 3.81)
-    include subdir-hooks.mk
-else
-    $(warning using GNU make $(MAKE_VERSION): hook targets not supported !)
-    include subdir-basic.mk
-endif
+.PHONY: subdirs $(SUBDIR)
+
+%: subdirs ;
+
+subdirs: $(SUBDIR)
+
+$(SUBDIR):
+	@$(MAKE) -C $@ $(MAKECMDGOALS)
 
 else    # MAKECMDGOALS == .help
 
@@ -38,27 +38,6 @@ else    # MAKECMDGOALS == .help
 	@echo " Available targets                                             "
 	@echo "-------------------                                            "
 	@echo "*any* target provided by the subdirectories' Makefile's        "
-	@echo
-ifeq ($(MAKE_VERSION), 3.81)
-	@echo "Each target T has T-pre and T-post companion targets.  These   "
-	@echo "(void) targets are at client's disposal and will always be     "
-	@echo "called before and after the associated target.                 "
-	@echo "A set of target with pattern <subdir>.<goal> is used internally"
-	@echo "to handle recursion.  Should you need to explicitly set a      "
-	@echo "given build dependency between subdirectories in any of the    "
-	@echo "goals, you must use the internal target format to express it.  "
-	@echo "E.g. to tell MaKL that the directory then_dir must be built    "
-	@echo "after the prerequisite_dir has been successfully built, you    "
-	@echo "will add the following line:                                   "
-	@echo "     then_dir.all: prerequisite_dir.all                        "
-	@echo "This is particularly important when doing parallel builds where"
-	@echo "the order in which subdirectories are specified in the SUBDIR  "
-	@echo "variable is not necessarily honoured by GNU make.              "
-else    # GNU make < 3.81
-	@echo "With GNU make version $(MAKE_VERSION), target hooking is not   "
-	@echo "supported.  Should you need this feature, upgrade to version   "
-	@echo "3.81.                                                          "
-endif   # GNU make == 3.81
 	@echo
 	@echo "---------------------                                          "
 	@echo " Available variables                                           "
