e1113ef93b8b2d8cc8077345996b4c0db7e68ffd
tho <tho>
2008-05-29 11:26:57 +0000

    [makl] ++obsession; a new subdir template

--- mk/subdir.mk
+++ mk/subdir.mk
@@ -1,4 +1,4 @@
-# $Id: subdir.mk,v 1.32 2008/05/21 16:14:46 tho Exp $
+# $Id: subdir.mk,v 1.33 2008/05/29 11:26:57 tho Exp $
 #
 # Variables:
 # - SUBDIR      A list of subdirectories that should be built as well.
@@ -19,14 +19,27 @@ ifeq ($(findstring .help, $(MAKECMDGOALS)),)
 .DEFAULT_GOAL := all
 MAKECMDGOALS ?= $(.DEFAULT_GOAL)
 
-.PHONY: subdirs $(SUBDIR)
+# all targets (plain and hooks)
+G = $(foreach T,$(MAKECMDGOALS),$(T)-pre $(addsuffix .$(T),$(SUBDIR)) $(T)-post)
 
-%: subdirs ;
+# the cartesian product between MAKECMDGOALS and SUBDIR sets
+G_PLAIN = $(filter-out %-pre %-post, $(G))
 
-subdirs: $(SUBDIR)
+# hook'd MAKECMDGOALS
+G_HOOK = $(filter %-pre %-post, $(G))
 
-$(SUBDIR):
-	@$(MAKE) -C $@ $(MAKECMDGOALS)
+.PHONY: $(G) goals
+
+$(MAKECMDGOALS): goals
+
+goals: $(G)
+
+# empty hooks (caller may override them)
+$(G_HOOK):
+
+# plain hooks (e.g. a_subdir.all, another_subdir.depend, ...)
+$(G_PLAIN):
+	@$(MAKE) -C $(basename $@) $(patsubst .%,%,$(suffix $@))
 
 else    # MAKECMDGOALS == .help
 
@@ -38,6 +51,21 @@ else    # MAKECMDGOALS == .help
 	@echo " Available targets                                             "
 	@echo "-------------------                                            "
 	@echo "*any* target provided by the subdirectories' Makefile's        "
+	@echo
+	@echo "Each target T has T-pre and T-post companion targets.  These   "
+	@echo "(void) targets are at client's disposal and will always be     "
+	@echo "called before and after the associated target.                 "
+	@echo "A set of target with pattern <subdir>.<goal> is used internally"
+	@echo "to handle recursion.  Should you need to explicitly set a      "
+	@echo "given build dependency between subdirectories in any of the    "
+	@echo "goals, you must use the internal target format to express it.  "
+	@echo "E.g. to tell MaKL that the directory then_dir must be built    "
+	@echo "after the prerequisite_dir has been successfully built, you    "
+	@echo "will add the following line:                                   "
+	@echo "     then_dir.all: prerequisite_dir.all                        "
+	@echo "This is particularly important when doing parallel builds where"
+	@echo "the order in which subdirectories are specified in the SUBDIR  "
+	@echo "variable is not necessarily honoured by GNU make.              "
 	@echo
 	@echo "---------------------                                          "
 	@echo " Available variables                                           "
