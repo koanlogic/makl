b03daf0be9f6e71fa8cf5ffea8892c0012485f1b
tho <tho>
2008-11-04 17:46:07 +0000

    [makl] added solaris shared libs build

--- shlib/solaris.mk
+++ shlib/solaris.mk
@@ -1,4 +1,4 @@
-# $Id: solaris.mk,v 1.1 2008/11/04 17:00:50 tho Exp $
+# $Id: solaris.mk,v 1.2 2008/11/04 17:46:07 tho Exp $
 #
 # Solaris
 
@@ -14,16 +14,14 @@ SHLIB_MINOR ?= 0
 .SUFFIXES: .so $(ALL_EXTS)
 
 $(foreach e,$(C_EXTS),$(addsuffix .so,$(e))):
-	$(CC) -fPIC $(CFLAGS) -c $< -o $*.so.o
-	$(LD) -X -r $*.so.o -o $*.so
-	rm -f $*.so.o
+	$(CC) -fPIC $(CFLAGS) -c $< -o $*.so
 
-$(foreach e,$(CXX_EXTS),$(addsuffix .so,$(e))):
-	$(CXX) -fPIC $(CXXFLAGS) -c $< -o $*.so.o
-	$(LD) -X -r $*.so.o -o $*.so
-	rm -f $*.so.o
-
-SHLIB_NAME ?= lib$(__LIB).so.$(SHLIB_MAJOR).$(SHLIB_MINOR)
+##
+## Set library naming vars
+##
+SHLIB_LINK ?= lib$(__LIB).so
+SONAME ?= $(SHLIB_LINK).$(SHLIB_MAJOR)
+SHLIB_NAME ?= $(SONAME).$(SHLIB_MINOR)
 
 ##
 ## build rules
@@ -32,17 +30,24 @@ all-shared: $(SHLIB_NAME)
 
 $(SHLIB_NAME): $(SHLIB_OBJS)
 	@echo "===> building shared $(__LIB) library"
-	rm -f $(SHLIB_NAME)
-	$(CC) -shared -fpic -o $(SHLIB_NAME) `$(LORDER) $(SHLIB_OBJS) | $(TSORT)`
+	rm -f $(SHLIB_NAME) $(SHLIB_LINK) $(SONAME)
+	ln -sf $(SHLIB_NAME) $(SHLIB_LINK)
+	ln -sf $(SHLIB_NAME) $(SONAME)
+	$(CC) -shared -o $(SHLIB_NAME) -Wl,-h,$(SONAME) \
+	`$(LORDER) $(SHLIB_OBJS) | $(TSORT)`
 
 install-shared:
 	$(INSTALL) $(__INSTALL_ARGS) -m $(LIBMODE) $(SHLIB_NAME) $(LIBDIR)
+	ln -sf $(SHLIB_NAME) $(LIBDIR)/$(SHLIB_LINK)
+	ln -sf $(SHLIB_NAME) $(LIBDIR)/$(SONAME)
 
 uninstall-shared:
 	rm -f $(LIBDIR)/$(SHLIB_NAME)
+	rm -f $(LIBDIR)/$(SHLIB_LINK)
+	rm -f $(LIBDIR)/$(SONAME)
 
 clean-shared:
 	rm -f $(SHLIB_OBJS)
-	rm -f $(SHLIB_NAME)
+	rm -f $(SHLIB_NAME) $(SHLIB_LINK) $(SONAME)
 
 endif
