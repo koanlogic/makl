b31d4b9f8a431ad1115e0c82a94cc88b164aa10d
tho <tho>
2010-02-17 17:33:07 +0000

    [makl] fbsd port += upload script

---
+++ pkgs/freebsd/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# get version
+v="`grep PORTVERSION Makefile | cut -d'=' -f2`"
+
+# strip trailing/leading blanks
+v=$(echo "$v" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+# build makl pkg name
+makl_pkg="makl-$v.tbz"
+
+# upload
+scp root@koanlogic.com:/var/www-anemic/www/download/makl/
